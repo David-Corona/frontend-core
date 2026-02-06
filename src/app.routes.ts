f66b61ff01ec1208f69aad773a6753ea9eb91c03
David Corona <corona_121@hotmail.com>
2026-02-06 21:35:22 +0100

    restructured fildes and routes to separate demo

--- src/app.routes.ts
+++ src/app.routes.ts
@@ -1,23 +1,37 @@
 import { Routes } from '@angular/router';
 import { AppLayout } from './app/layout/component/app.layout';
-import { Dashboard } from './app/pages/dashboard/dashboard';
-import { Documentation } from './app/pages/documentation/documentation';
-import { Landing } from './app/pages/landing/landing';
-import { Notfound } from './app/pages/notfound/notfound';
+
+// Demo
+import { Dashboard } from './app/demo/pages/dashboard/dashboard';
+import { Documentation } from './app/demo/pages/documentation/documentation';
+import { Landing } from './app/demo/pages/landing/landing';
+import { Notfound } from './app/demo/pages/notfound/notfound';
 
 export const appRoutes: Routes = [
     {
         path: '',
         component: AppLayout,
         children: [
-            { path: '', component: Dashboard },
-            { path: 'uikit', loadChildren: () => import('./app/pages/uikit/uikit.routes') },
-            { path: 'documentation', component: Documentation },
-            { path: 'pages', loadChildren: () => import('./app/pages/pages.routes') }
+
+
+            // Demo routes
+            {
+                path: 'demo',
+                children: [
+                    { path: '', component: Dashboard },
+                    { path: 'dashboard', component: Dashboard },
+                    { path: 'uikit', loadChildren: () => import('./app/demo/pages/uikit/uikit.routes') },
+                    { path: 'documentation', component: Documentation },
+                    { path: 'pages', loadChildren: () => import('./app/demo/pages/demo.routes') },
+                ]
+            }
+
         ]
     },
-    { path: 'landing', component: Landing },
+    { path: 'demo/auth', loadChildren: () => import('./app/demo/pages/auth/auth.routes') },
+    { path: 'demo/landing', component: Landing },
+
+    // Error pages
     { path: 'notfound', component: Notfound },
-    { path: 'auth', loadChildren: () => import('./app/pages/auth/auth.routes') },
     { path: '**', redirectTo: '/notfound' }
 ];
